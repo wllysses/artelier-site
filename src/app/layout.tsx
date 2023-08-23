0a723399b68b406bb811ba145023ac5cd7a95cb1
wllysses <wllyssestavares@gmail.com>
2023-08-22 21:20:38 -0300

    refactor: refactor layout.page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ToastContainer } from "react-toastify";
@@ -9,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const poppins = Poppins({ subsets: ['latin'], weight: ['200', '300', '400', '700'] })
 
 export const metadata: Metadata = {
-  title: 'Artelier - Artesanato Personalizado',
+  title: 'Artelier - Quadros & Personalizados',
   description: 'Somos o Artelier, uma loja virtual pensada para transformar seus sonhos em arte.',
 }
 
